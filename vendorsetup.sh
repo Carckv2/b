fe9147fd78b6c203c48f63cd3bfab24c3c6c5c83
Carckv2 <Ankitshresthaas2@mail.com>
2023-03-01 01:43:18 -0500

    first commit

---
+++ vendorsetup.sh
@@ -0,0 +1,17 @@
+# Vendor tree
+git clone https://github.com/drtsinx98/proprietary_vendor_realme_spaced vendor/realme/spaced --depth=1
+
+
+# Vendor-ims tree
+git clone https://github.com/drtsinx98/proprietary_vendor_realme_ims-spaced vendor/realme/ims-spaced --depth=1
+
+
+# Kernel tree
+git clone --depth=1 https://github.com/drtsinx98/android_kernel_realme_mt6781 kernel/realme/spaced
+
+# frameworks/av (required for sysbta)
+git clone --depth=1 https://github.com/rk134/frameworks_av.git frameworks/av
+
+
+# packages/modules/Bluetooth (required for sysbta)
+git clone --depth=1 https://github.com/rk134/packages_modules_Bluetooth.git packages/modules/Bluetooth
